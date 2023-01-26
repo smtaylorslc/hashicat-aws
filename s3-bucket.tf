abf1c73e4fc3f40edb2496fdcd92993544faaa9e
Spencer Taylor <taylor.spencer@heb.com>
2023-01-26 18:52:20 +0000

    Removing bucket param

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/spencertaylor-heb-test-org/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "my-s3-bucket"
   bucket_prefix = var.prefix
   acl    = "private"
 
